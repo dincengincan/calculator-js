8e7c600a2ac349a9093fd60f9d957931954d370c
dincengincan <engincandinc@gmail.com>
2020-01-22 19:25:12 +0300

    bug fixed 2

--- App.js
+++ App.js
@@ -2,14 +2,15 @@ const buttons = document.querySelector(".container");
 const screen = document.querySelector(".screen");
 
 let firstNumber = "";
-let calculation = "";
+let operator = null;
 
 buttons.addEventListener("click", function(event) {
   if (
     event.target.className === "button number" ||
     event.target.className === "button zero number"
-  )
+  ) {
     display(event);
+  }
   event.target.className === "button plus" && calculate("plus");
   event.target.className === "button minus" && calculate("minus");
   event.target.className === "button divide" && calculate("divide");
@@ -17,7 +18,6 @@ buttons.addEventListener("click", function(event) {
   event.target.className === "button equals" && equals();
   event.target.className === "button backspace" && backspace();
   event.target.className === "button clear" && clear();
-  event.stopPropagation();
 });
 
 function display(event) {
@@ -34,32 +34,35 @@ function display(event) {
 
 function clear() {
   screen.innerText = 0;
+  operator = null;
 }
 
 function backspace() {
   let newResult = screen.innerText.substring(0, screen.innerText.length - 1);
   if (newResult.length === 0) {
-    resscreenult.innerText = 0;
+    screen.innerText = 0;
   } else {
     screen.innerText = newResult;
   }
+  operator = null;
 }
 
 function equals() {
   let secondNumber = parseInt(screen.innerText);
-  if (calculation === "plus") {
+  if (operator === "plus") {
     screen.innerText = firstNumber + secondNumber;
-  } else if (calculation === "minus") {
+  } else if (operator === "minus") {
     screen.innerText = firstNumber - secondNumber;
-  } else if (calculation === "multiply") {
+  } else if (operator === "multiply") {
     screen.innerText = (firstNumber * secondNumber).toString().substring(0, 16);
-  } else if (calculation === "divide") {
+  } else if (operator === "divide") {
     screen.innerText = (firstNumber / secondNumber).toString().substring(0, 16);
   }
+  operator = null;
 }
 
 function calculate(type) {
   firstNumber = parseInt(screen.innerText);
   screen.innerText = 0;
-  calculation = type;
+  operator = type;
 }
